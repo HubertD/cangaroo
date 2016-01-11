79684fb3393bfe5708b3ab69b8e0b05891ca8684
Hubert Denkmair <hubert@denkmair.de>
2016-01-11 21:58:57 +0100

    add proper gpl header to all source files

--- src/model/CanMessage.h
+++ src/model/CanMessage.h
@@ -1,9 +1,23 @@
 /*
- * CanMessage.h
- *
- *  Created on: 20.12.2015
- *      Author: hd
- */
+
+  Copyright (c) 2015, 2016 Hubert Denkmair <hubert@denkmair.de>
+
+  This file is part of cangaroo.
+
+  cangaroo is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  cangaroo is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with cangaroo.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 
 #ifndef BASE_CANMESSAGE_H_
 #define BASE_CANMESSAGE_H_
