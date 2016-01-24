706b87f0a86101c566c33734b61f4b279349fc23
Hubert Denkmair <hubert@denkmair.de>
2016-01-24 16:17:35 +0100

    update readme

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -405,7 +405,7 @@ void MainWindow::showAboutDialog()
        "cangaroo\n"
        "open source can bus analyzer\n"
        "\n"
-       "version 0.2.0\n"
+       "version 0.2.1\n"
        "\n"
        "(c)2015-2016 Hubert Denkmair"
     );
